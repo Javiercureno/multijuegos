69159f7434087be4ee9f4d9e6d7b420a28ad8333
Javier Cureño <37353310+Javiercureno@users.noreply.github.com>
2025-04-10 21:07:32 -0600

    Update HomeController.java
    contador

--- src/main/java/com/ebc/multijuegos/controller/HomeController.java
+++ src/main/java/com/ebc/multijuegos/controller/HomeController.java
@@ -8,8 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/") // Define la raíz como ruta base
 public class HomeController {
 
+    // Variable estática para contar las visitas
+    private static int visitCount = 0;
+
+    // Método para manejar las peticiones GET a la raíz
     @GetMapping
     public String home() {
-        return "¡Hola CI/CD..... :) Jenkins lee hooks!";
+        // Incrementa el contador de visitas
+        visitCount++;
+        
+        // Mensaje de bienvenida con el número de visitas
+        return "¡Hola CI/CD con Jenkins y Git Hooks! Bienvenido al homepage de MultiJuegos. "
+                + "Este sitio ha sido visitado " + visitCount + " veces.";
     }
 }
