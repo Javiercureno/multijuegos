586fe2262c6f86655a77f3f6c229d67ae052f62b
Javier Cureño <37353310+Javiercureno@users.noreply.github.com>
2025-04-10 21:46:13 -0600

    Update HomeController.java

--- src/main/java/com/ebc/multijuegos/controller/HomeController.java
+++ src/main/java/com/ebc/multijuegos/controller/HomeController.java
@@ -1,3 +1,8 @@
+package com.ebc.multijuegos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
