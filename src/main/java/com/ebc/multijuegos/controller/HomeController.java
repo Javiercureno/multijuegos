a012ea923f0b8ba4abcf358cb5301f6ac3a77c77
Javier Cureño <37353310+Javiercureno@users.noreply.github.com>
2025-04-10 21:10:49 -0600

    Update HomeController.java
    html

--- src/main/java/com/ebc/multijuegos/controller/HomeController.java
+++ src/main/java/com/ebc/multijuegos/controller/HomeController.java
@@ -20,5 +20,14 @@ public class HomeController {
         // Mensaje de bienvenida con el número de visitas
         return "¡Hola CI/CD con Jenkins y Git Hooks! Bienvenido al homepage de MultiJuegos. "
                 + "Este sitio ha sido visitado " + visitCount + " veces.";
+
+        // Mensaje HTML con estilo: cambiamos el tamaño de la fuente y el color
+        return "<html>" +
+               "<body>" +
+               "<h1 style='font-size: 30px; color: #4CAF50;'>¡Hola CI/CD con Jenkins y Git Hooks!</h1>" +
+               "<p style='font-size: 20px; color: #0000FF;'>Bienvenido al homepage de MultiJuegos. " +
+               "Este sitio ha sido visitado " + visitCount + " veces.</p>" +
+               "</body>" +
+               "</html>";
     }
 }
