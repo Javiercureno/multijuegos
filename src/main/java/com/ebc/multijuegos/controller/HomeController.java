8f67af302a061bfee6959a0204f1dcf2ad2065ce
Javier Cureño <37353310+Javiercureno@users.noreply.github.com>
2025-04-10 21:30:14 -0600

    Update HomeController.java

--- src/main/java/com/ebc/multijuegos/controller/HomeController.java
+++ src/main/java/com/ebc/multijuegos/controller/HomeController.java
@@ -1,25 +1,14 @@
-package com.ebc.multijuegos.controller;
+import java.util.Scanner;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/") // Define la raíz como ruta base
-public class HomeController {
-
-    // Variable estática para contar las visitas
-    private static int visitCount = 0;
-
-    // Método para manejar las peticiones GET a la raíz
-    @GetMapping
-    public String home() {
-        // Incrementa el contador de visitas
-        visitCount++;
-        
-        // Mensaje de bienvenida con el número de visitas
-        return "¡Hola CI/CD con Jenkins y Git Hooks! Bienvenido al homepage de MultiJuegos. "
-                + "Este sitio ha sido visitado " + visitCount + " veces.";
-
+class AreaOfCircle
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the radius:");
+        double r = s.nextDouble();
+        double area = (22*r*r)/7 ;
+        System.out.println("Area of Circle is: " + area);
     }
 }
+
